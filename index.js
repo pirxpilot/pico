d606233ace54d02e61ed76d76769b1daae5ed814
Damian Krzeminski <pirxpilot@furkot.com>
2025-06-23 08:52:56 -0600

    export each tool function as named export

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import events from './lib/events.js';
 import observer from './lib/observer.js';
 import pico from './lib/pico.js';
 import template from './lib/template.js';
-import tools from './lib/tools.js';
+import tools, { initTemplateCache, fromTemplate, replaceSlots } from './lib/tools.js';
 
 export default {
   pico,
@@ -14,4 +14,4 @@ export default {
   tools
 };
 
-export { pico, cache, events, observer, template, tools };
+export { pico, cache, events, observer, template, initTemplateCache, fromTemplate, replaceSlots };
